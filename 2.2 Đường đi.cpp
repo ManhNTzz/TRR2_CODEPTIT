ee4d2f605d30e4c8464d5520e70a8b20af830f04
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-04 16:39:34 +0700

    TRR2

--- 2.2 Đường đi.cpp
+++ 2.2 Đường đi.cpp
@@ -9,20 +9,17 @@ int n, st, en, cnt = 0;
 int a[maxn][maxn] = {}, path[maxn] = {};
 bool vis[maxn] = {};
 
-void DFS(int u, int d)
-{
+void DFS(int u, int d) {
     if (d > 2)
         return;
 
-    if (d == 2 && u == en)
-    {
+    if (d == 2 && u == en) {
         cnt++;
         return;
     }
 
     vis[u] = true;
-    for (int v = 1; v <= n; v++)
-    {
+    for (int v = 1; v <= n; v++) {
         if (!vis[v] && a[u][v])
         {
             DFS(v, d + 1);
@@ -34,28 +31,23 @@ void DFS(int u, int d)
 bool ok = false;
 vector<int> res;
 
-void BFS()
-{
+void BFS() {
     queue<pair<int, vector<int>>> qe;
     qe.push({st, {st}});
 
-    while (!qe.empty())
-    {
+    while (!qe.empty()){
         pair<int, vector<int>> top = qe.front();
         qe.pop();
         int u = top.first;
         vector<int> vi = top.second;
 
-        if (u == en)
-        {
+        if (u == en){
             res = vi;
             return;
         }
         vis[u] = true;
-        for (int v = 1; v <= n; v++)
-        {
-            if (!vis[v] && a[u][v])
-            {
+        for (int v = 1; v <= n; v++) {
+            if (!vis[v] && a[u][v]) {
                 vi.push_back(v);
                 qe.push({v, vi});
                 vi.pop_back();
@@ -64,8 +56,7 @@ void BFS()
     }
 }
 
-int main()
-{
+int main(){
     // freopen("TK.INP", "r", stdin);
     // freopen("TK.OUT", "w", stdout);
     ios_base::sync_with_stdio(0);
@@ -75,26 +66,21 @@ int main()
     cin >> k;
     cin >> n >> st >> en;
 
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
             cin >> a[i][j];
         }
     }
 
-    if (k == 1)
-    {
+    if (k == 1) {
         DFS(st, 0);
         cout << cnt << endl;
     }
-    else
-    {
+    else {
         BFS();
         if (res.size() == 0)
             cout << "0" << endl;
-        else
-        {
+        else {
             for (int x : res)
                 cout << x << " ";
         }
