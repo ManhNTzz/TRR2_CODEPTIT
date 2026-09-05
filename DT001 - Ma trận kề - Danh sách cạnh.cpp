659095e36caf9328a1644ab3bed66fa4a747c5f0
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-05 14:27:32 +0700

    TRR2

--- DT001 - Ma trận kề - Danh sách cạnh.cpp
+++ DT001 - Ma trận kề - Danh sách cạnh.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 // Cre by ManhNTzz
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -16,18 +15,15 @@ int main()
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
             cin >> a[i][j];
-    if (t == 1)
-    {
-        for (int i = 1; i <= n; i++)
-        {
+    if (t == 1) {
+        for (int i = 1; i <= n; i++) {
             int deg = 0;
             for (int j = 1; j <= n; j++)
                 deg += a[i][j];
             cout << deg << " ";
         }
     }
-    else if (t == 2)
-    {
+    else if (t == 2) {
         vector<pair<int, int>> edge;
         for (int i = 1; i <= n; i++)
             for (int j = i + 1; j <= n; j++)
