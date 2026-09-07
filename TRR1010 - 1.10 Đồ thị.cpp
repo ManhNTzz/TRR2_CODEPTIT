fe26d755b1e412b93bf6d07eac570cd2de27e0c4
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-07 14:12:29 +0700

    TRR2

--- TRR1010 - 1.10 Đồ thị.cpp
+++ TRR1010 - 1.10 Đồ thị.cpp
@@ -21,8 +21,7 @@ set<vector<int>> dsc;
 int bac[105] = {0};
 int t, n;
 int a[105][105] = {};
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -30,12 +29,10 @@ int main()
     ofstream cout("DT.OUT");
     cin >> t >> n;
     FOR(i, 1, n)
-    FOR(j, 1, n)
-    {
+    FOR(j, 1, n) {
         int x;
         cin >> x;
-        if (x > 0 && x <= 50 && j >= i + 1)
-        {
+        if (x > 0 && x <= 50 && j >= i + 1) {
             a[i][j] = x;
             bac[i]++;
             bac[j]++;
@@ -45,11 +42,9 @@ int main()
     if (t == 1)
         FOR(i, 1, n)
     cout << bac[i] << " ";
-    else
-    {
+    else {
         cout << n << " " << dsc.size() << endl;
-        for (auto v : dsc)
-        {
+        for (auto v : dsc) {
             cout << v[0] << " " << v[1] << " " << a[v[0]][v[1]] << endl;
         }
     }
