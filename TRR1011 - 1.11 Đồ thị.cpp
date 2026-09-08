111161498f56d302f4df478c002c94e8750e91e2
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-08 14:39:26 +0700

    TRR2

--- TRR1011 - 1.11 Đồ thị.cpp
+++ TRR1011 - 1.11 Đồ thị.cpp
@@ -20,16 +20,14 @@ const int INF = 1e9;
 vector<vector<int>> a(105, vector<int>(105, 10000));
 int bac[105] = {};
 int t, n, m;
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     ifstream cin("DT.INP");
     ofstream cout("DT.OUT");
     cin >> t >> n >> m;
-    REP(i, m)
-    {
+    REP(i, m) {
         int x, y, z;
         cin >> x >> y >> z;
         a[x][y] = a[y][x] = z;
@@ -39,13 +37,10 @@ int main()
     if (t == 1)
         FOR(i, 1, n)
     cout << bac[i] << " ";
-    else
-    {
+    else {
         cout << n << endl;
-        FOR(i, 1, n)
-        {
-            FOR(j, 1, n)
-            {
+        FOR(i, 1, n) {
+            FOR(j, 1, n) {
                 if (i == j)
                     cout << 0 << " ";
                 else
