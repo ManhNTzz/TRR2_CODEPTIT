4cea62352039689ee2427c6b9a4de53d99b7b132
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-11 08:32:38 +0700

    TRR2

--- TRR3004 - 3.4 Chu trình Euler.cpp
+++ TRR3004 - 3.4 Chu trình Euler.cpp
@@ -20,17 +20,14 @@ const int INF = 1e9;
 int n, u;
 int a[105][105];
 bool vs[105];
-void DFS(int u)
-{
+void DFS(int u) {
     vs[u] = true;
-    FOR(i, 1, n)
-    {
+    FOR(i, 1, n) {
         if (!vs[i] && a[u][i])
             DFS(i);
     }
 }
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -38,18 +35,15 @@ int main()
     ofstream cout("CT.OUT");
     int t;
     cin >> t;
-    if (t == 1)
-    {
+    if (t == 1) {
         cin >> n;
         FOR(i, 1, n)
         FOR(j, 1, n)
             cin >> a[i][j];
         memset(vs, false, sizeof(vs));
         DFS(1);
-        FOR(i, 1, n)
-        {
-            if (!vs[i])
-            {
+        FOR(i, 1, n) {
+            if (!vs[i]) {
                 cout << 0;
                 return 0;
             }
@@ -57,17 +51,14 @@ int main()
         int vao[105] = {0};
         int ra[105] = {0};
         FOR(i, 1, n)
-        FOR(j, 1, n)
-        {
-            if (a[i][j])
-            {
+        FOR(j, 1, n) {
+            if (a[i][j]) {
                 vao[j]++;
                 ra[i]++;
             }
         }
         int x = 0, y = 0;
-        FOR(i, 1, n)
-        {
+        FOR(i, 1, n) {
             if (vao[i] - ra[i] == 1)
                 x++;
             else if (ra[i] - vao[i] == 1)
@@ -80,36 +71,30 @@ int main()
         else
             cout << 0;
     }
-    else
-    {
+    else {
         cin >> n >> u;
         FOR(i, 1, n)
         FOR(j, 1, n)
             cin >> a[i][j];
         stack<int> st, ce;
         st.push(u);
-        while (!st.empty())
-        {
+        while (!st.empty()) {
             int x = st.top();
             int ok = 0;
-            FOR(i, 1, n)
-            {
-                if (a[x][i])
-                {
+            FOR(i, 1, n) {
+                if (a[x][i]) {
                     st.push(i);
                     a[x][i] = 0;
                     ok = 1;
                     break;
                 }
             }
-            if (!ok)
-            {
+            if (!ok) {
                 st.pop();
                 ce.push(x);
             }
         }
-        while (!ce.empty())
-        {
+        while (!ce.empty()) {
             cout << ce.top() << " ";
             ce.pop();
         }
