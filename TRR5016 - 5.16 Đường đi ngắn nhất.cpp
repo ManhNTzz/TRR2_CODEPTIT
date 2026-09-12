c7fb66cad4180a6fac2b99eea5e660e231c10c25
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-12 08:22:04 +0700

    TRR2

--- TRR5016 - 5.16 Đường đi ngắn nhất.cpp
+++ TRR5016 - 5.16 Đường đi ngắn nhất.cpp
@@ -19,8 +19,7 @@ const int INF = 1e9;
 
 int a[105][105], Next[105][105], d[105][105];
 int n;
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -28,12 +27,10 @@ int main()
     ofstream cout("DN.OUT");
     cin >> n;
     FOR(i, 1, n)
-    FOR(j, 1, n)
-    {
+    FOR(j, 1, n) {
         cin >> a[i][j];
         d[i][j] = a[i][j];
-        if (i != j && a[i][j] != 10000)
-        {
+        if (i != j && a[i][j] != 10000) {
             Next[i][j] = j;
         }
         else
@@ -41,31 +38,25 @@ int main()
     }
     FOR(k, 1, n)
     FOR(i, 1, n)
-    FOR(j, 1, n)
-    {
-        if (d[i][j] > d[i][k] + d[k][j])
-        {
+    FOR(j, 1, n) {
+        if (d[i][j] > d[i][k] + d[k][j]) {
             d[i][j] = d[i][k] + d[k][j];
             Next[i][j] = Next[i][k];
         }
     }
     int u = -1, v = -1, max_val = 0;
     FOR(i, 1, n)
-    FOR(j, 1, n)
-    {
-        if (i != j && d[i][j] != 10000 && d[i][j] > max_val)
-        {
+    FOR(j, 1, n) {
+        if (i != j && d[i][j] != 10000 && d[i][j] > max_val) {
             max_val = d[i][j];
             u = i;
             v = j;
         }
     }
-    if (u != -1)
-    {
+    if (u != -1) {
         cout << u << " " << v << " " << max_val << endl;
         int curr = u;
-        while (1)
-        {
+        while (1) {
             cout << curr << " ";
             if (curr == v)
                 break;
