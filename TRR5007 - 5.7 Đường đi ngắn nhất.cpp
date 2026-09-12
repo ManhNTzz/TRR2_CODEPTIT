73122fcff08f042f6fc29618daaea00a84ffc77a
Nguyen Tien Manh <toilamanh16@gmail.com>
2026-09-12 08:21:21 +0700

    TRR2

--- TRR5007 - 5.7 Đường đi ngắn nhất.cpp
+++ TRR5007 - 5.7 Đường đi ngắn nhất.cpp
@@ -15,8 +15,7 @@ const long long mod = 1000000007;
 
 int a[105][105], d[105], truoc[105], vs[105];
 int n, s, t;
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -27,24 +26,18 @@ int main()
     FOR(i, 1, n)
     FOR(j, 1, n)
         cin >> a[i][j];
-    FOR(i, 1, n)
-    {
+    FOR(i, 1, n) {
         d[i] = 10000;
         truoc[i] = 0;
     }
     d[s] = 0;
     int k = 1;
-    while (k <= n - 1)
-    {
+    while (k <= n - 1) {
         bool update = false;
-        FOR(i, 1, n)
-        {
-            FOR(j, 1, n)
-            {
-                if (d[j] != 10000 && a[j][i] != 10000 && a[j][i] != 0)
-                {
-                    if (d[i] > d[j] + a[j][i])
-                    {
+        FOR(i, 1, n) {
+            FOR(j, 1, n) {
+                if (d[j] != 10000 && a[j][i] != 10000 && a[j][i] != 0) {
+                    if (d[i] > d[j] + a[j][i]) {
                         d[i] = d[j] + a[j][i];
                         truoc[i] = j;
                         update = true;
@@ -58,10 +51,8 @@ int main()
     }
     int checkchutrinham = 0;
     FOR(i, 1, n)
-    FOR(j, 1, n)
-    {
-        if (d[i] > d[j] + a[j][i])
-        {
+    FOR(j, 1, n) {
+        if (d[i] > d[j] + a[j][i]) {
             checkchutrinham = 1;
             break;
         }
@@ -70,17 +61,14 @@ int main()
         cout << -1;
     else if (d[t] == 10000)
         cout << 0;
-    else
-    {
+    else {
         cout << d[t] << endl;
         vector<int> duongdi;
         int curr = t;
-        while (1)
-        {
+        while (1) {
             duongdi.push_back(curr);
             curr = truoc[curr];
-            if (curr == s)
-            {
+            if (curr == s) {
                 duongdi.push_back(s);
                 break;
             }
